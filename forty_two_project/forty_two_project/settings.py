0b92b569489bdc61f10c8f4ee16f28baa92b30c3
Max Kolle <2247156k@student.gla.ac.uk>
2018-07-08 01:35:06 +0100

    Add materialise css, jquery, base.js. Add navbar, basic layouts, tabs for searching for questions and giving answers

--- forty_two_project/forty_two_project/settings.py
+++ forty_two_project/forty_two_project/settings.py
@@ -17,6 +17,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+
+STATICFILES_DIRS = [
+    STATIC_PATH,
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
---
+++ forty_two_project/static/js/base.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    $('.tabs').tabs();  // Give tabs interactivity
+    $('.sidenav').sidenav();  // Add a sidenav menu for when the window is narrow.
+});
